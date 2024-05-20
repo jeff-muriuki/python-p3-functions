bd71df17dd0b8a649c891d2ce5dd1ae5b491161c
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-05-20 12:50:11 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,32 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print('Hello, programmer!')
 
-def greet(name):
-    pass
+greet_programmer()
+
+
+def greet(jeff):
+    print(f'Hello, {jeff}!')
+
+greet('jeff')
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}!')
+    
+greet_with_default('muriuki')    
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+add(1,2)
+
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    
+    return number / 2
+
+halve(10)
